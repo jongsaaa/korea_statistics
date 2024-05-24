facb3cc47877fa6085dc1b76d42c2bea0d210718
jongsaaa <asuna7714@gmail.com>
2024-05-24 17:06:41 +0900

    init

---
+++ _config.py
@@ -0,0 +1,19 @@
+#encoding
+euc_kr = 'CP949'
+utf = 'utf-8'
+
+#layer target year
+layer_years = [2000, 2005, 2010, 2015, 2020, 2023]
+
+#인구
+pop_read_path = '.\\pop\\read'
+pop_write_path = '.\\pop\\write'
+
+#인프라
+infra_read_path = '.\\infra\\read'
+infra_write_path = '.\\infra\\write'
+
+#학교
+school_read_path = '.\\school\\read'
+school_write_path = '.\\school\\write'
+
